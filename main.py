7afb131fad7028c92e536116e0ca6d88bb77ef2e
SamraAzizi <samarraazizi@gmail.com>
2025-04-20 21:02:50 +0500

    automate finances with python

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd 
+import plotly.express as px 
+import json 
+import os
+
+
+st.set_page_config(page_title ="Simple Finance App", page_icon="💰", layout="wide")
+
+
+def main():
+    st.title("Simple Finance Dashboard")
+
+    uploaded_file = st.file_uploader("Uploaded your transaction CSV file", type=["csv"])
