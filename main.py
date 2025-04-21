80ad0efddbf44becfbf0be5e7288e1f8daad9c51
SamraAzizi <samarraazizi@gmail.com>
2025-04-21 11:55:45 +0500

    automate finances with python

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def load_transactions(file):
         df = pd.read_csv(file)
         df.columns = [col.strip() for col in df.columns]
         df["Amount"] = df["Amount"].str.replace(",", "").astype(float)
+        df["Date"] = pd.to_datetime(df["Date"], format="%d %b %Y")
         st.write(df)
 
         return df
@@ -28,6 +29,17 @@ def main():
 
     if uploaded_file is not None:
         df = load_transactions(uploaded_file)
+
+        if df is not None:
+            debits_df = df[df["Debit/Credit"] == "Debit"].copy()
+            credits_df = df[df["Debit/Credit"] == "Credit"].copy()
+
+            tab1, tab2 =st.tabs(["Expense ( Debits)", "Payements ( Credits)"])
+            with tab1:
+                st.write(debits_df)
+
+            with tab2:
+                st.write(credits_df)
 
 
 
